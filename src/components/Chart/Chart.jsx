a311762054a0837406af74208cc7d5061ce0ffa7
Triston <tristonherbst97@gmail.com>
2021-07-09 16:04:00 -0500

    getting dailydata from api

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Line, Bar } from 'react-chartjs-2'
+
+import {fetchDailyData} from '../../api'
+import styles from './Chart.module.css'
 
 function Chart(props) {
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        const fetchedDailyData = async () => {
+            setDailyData(await fetchDailyData())
+        }
+
+        fetchedDailyData()
+    },)
+
+    const lineChart = (
+        <Line 
+        data={{
+            labels: '',
+            datasets: [{}, {}]
+        }}
+
+        />
+    )
+
     return (
         <div>
             Chart
