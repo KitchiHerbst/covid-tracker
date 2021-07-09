24fb2bb052c45620a2a134ccb5be5c3f9bc8d6f6
Triston <tristonherbst97@gmail.com>
2021-07-09 14:30:30 -0500

    first card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
 
-function Cards(props) {
+import styles from './Cards.module.css'
+
+
+function Cards({ data }) {
+
+
     return (
-        <div>
-            Cards
+        <div className={styles.container}>
+            <Grid container spacing={3} justify-content='center'>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5' >REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='body2'>number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
         </div>
     );
 }
