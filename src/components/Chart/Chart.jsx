c3b53517704307d113b620f9a8680db6dad28343
Triston <tristonherbst97@gmail.com>
2021-07-09 16:31:57 -0500

    chart completed

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import { registerables } from 'chart.js';
+import React, { useState, useEffect } from 'react';
 import { Line, Bar } from 'react-chartjs-2'
 
-import {fetchDailyData} from '../../api'
+import { fetchDailyData } from '../../api'
 import styles from './Chart.module.css'
 
 function Chart(props) {
-    const [dailyData, setDailyData] = useState({})
+    const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
         const fetchedDailyData = async () => {
@@ -13,21 +14,37 @@ function Chart(props) {
         }
 
         fetchedDailyData()
-    },)
+    }, [])
+
+   
 
     const lineChart = (
-        <Line 
-        data={{
-            labels: '',
-            datasets: [{}, {}]
-        }}
+        dailyData.length ?
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: 'Infected',
+                        borderColor: '#3333ff',
+                        fill: true
+                    }, {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: 'Deaths',
+                        borderColor: 'red',
+                        backgroundColor: 'rgba(255, 0,0,.5)',
+                        fill: true
+                    }]
+                }}
 
-        />
+            /> : null
     )
 
+    console.log(dailyData)
+
     return (
-        <div>
-            Chart
+        <div className={styles.container}>
+            {lineChart}
         </div>
     );
 }
